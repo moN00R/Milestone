1560ac930e6219a169ed0f0e844723318a1ce25d
unknown <nooralhasan882@gmail.com>
2024-03-14 11:58:19 +0300

    make the git method by param

--- account_user/urls.py
+++ account_user/urls.py
@@ -3,8 +3,9 @@ from . import views
 from django.urls import path
 
 router = routers.DefaultRouter()
-router.register('user', views.get_user)
+router.register(
+    'user/<name:char>/<ERB_Student_Id:char>/<phonenumber:char>/<password:char>', views.get_user_info)
 
 urlpatterns = router.urls + [
-    # path('User/', views.User_info),
+
 ]
--- account_user/views.py
+++ account_user/views.py
@@ -2,11 +2,19 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .serializers import UserinfoSerializer
 from .models import User_info
-# Create your views here.
 
 
-class get_user(ModelViewSet):
+class get_user_info(ModelViewSet):
     http_method_names = ['get']
     queryset = User_info.objects.all()
     serializer_class = UserinfoSerializer
     permission_classes = ()
+
+    def list(self, request, *args, **kwargs):
+        data = super().list(request, *args, **kwargs)
+        return data
+    
+    
+    
+    
+
