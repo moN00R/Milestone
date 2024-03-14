5057133d9411745b3a6818a9580d6df4fbece236
unknown <nooralhasan882@gmail.com>
2024-03-14 12:09:59 +0300

    fix the url

--- account_user/urls.py
+++ account_user/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 
 router = routers.DefaultRouter()
 router.register(
-    'user/<name:char>/<ERB_Student_Id:char>/<phonenumber:char>/<password:char>', views.get_user_info)
+    '<name:char>/<ERB_Student_Id:char>/<phonenumber:char>/<password:char>', views.get_user_info)
 
 urlpatterns = router.urls + [
 
