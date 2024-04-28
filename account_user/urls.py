4d1b7158253884a638d1dd6f0a25a7c32b97c0df
noor <nooralhasan882@gmail.com>
2024-04-28 09:38:37 +0300

    test

--- account_user/urls.py
+++ account_user/urls.py
@@ -9,7 +9,7 @@ router.register('signup', SignUpView, basename='signup')
 urlpatterns = router.urls + [
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', GetUserInfoView.as_view(), name='get_user_info'),
-    path('get-user-info-details/<str:assessment_result>', GetUserDetailsCourse.as_view(), name='Get_user_info_details'),
+    # path('get-user-info-details/<str:assessment_result>', GetUserDetailsCourse.as_view(), name='Get_user_info_details'),
     path('change-password/', ChangePasswordView.as_view(), name='change_password'),
     path('', include(router.urls))
 ]
